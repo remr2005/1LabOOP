9ec74dadad6cbb88e5286e7d3433519ad52375dc
Kemran <morpeh2005ka@gmail.com>
2024-09-14 15:33:17 +0300

    Сделал всю графическую часть первого задания

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,10 +19,32 @@ namespace _1LabOOP
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TxtBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // если пользователь вводит что угодно кроме "0123456789-," или
+            // пользователь вводит - и при этом строка не пустая или
+            // пользователь вводит запятую когда строка пустая или когда уже содержит ","
+            // отменить ввод
+            if (!"0123456789-,".Contains(e.Text) || (e.Text == "-" && TxtBox.Text.Length != 0) || (e.Text == "," && (TxtBox.Text.Length == 0 || TxtBox.Text.Contains(","))))
+            {
+                e.Handled = true;
+            }
+        }
+        // к сожалению PrewiewTextInput не обрабатывает не символьные клавиши, пробел в том числе, поэтому что бы пользователь не мог ввести пробел испоьзуется PrewiewKeyDown
+        private void InputTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // Проверяем, нажата ли клавиша пробела
+            if (e.Key == Key.Space)
+            {
+                // Если пробел, то запрещаем ввод
+                e.Handled = true;
+            }
         }
     }
 }
